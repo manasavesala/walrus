439244c4b1b3becd739254f939999167ad42c02d
fausto guillen and manasa vesala <manasa@>
2018-10-16 10:19:40 -0700

    add bootstrap, background button functionality with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
     });
   });
 
+  $(document).ready(function() {
+    $("button#green").click(function() {
+      $("body").removeClass();
+      $("body").addClass("green-background");
+    });
+  
+    $("button#yellow").click(function() {
+      $("body").removeClass();
+      $("body").addClass("yellow-background");
+    });
+  
+    $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+    });
+  });
+  

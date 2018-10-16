4663659730ecb4a252fb47e453a94457664ba98f
fausto guillen and manasa vesala <fausto@>
2018-10-16 09:48:23 -0700

    add css , img ,index, jquery

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".click").click(function() {
+        $(".walrus-showing").toggle();
+        $(".walrus-hidden").toggle();
+    });    
+
+    $(".click1").click(function() {
+        $("#imgs-show").slideToggle();
+    });
+  });
+
